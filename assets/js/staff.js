8c167cdf6533efae1da9bedbd3636e13c27a8e4a
Henry Campos <henry.campos@galileo.edu>
2019-07-07 14:57:22 -0600

    Version 1.1.0

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -4,56 +4,70 @@ var staff = [
         information : 'Innovation Lab leader and Google Developer Expert.',
         image : 'adrian.jpg',
         job : 'boss',
-        email : 'adriancatalan@galileo.edu'
+        email : 'adriancatalan@galileo.edu',
+        emoji: '🙊'
     },
     {
         name : 'Luis Jimenez',
         information : 'Interested in computer vision, hardware and education.',
-        image : 'luis.jpg',
+        image : 'luis2.jpg',
+        gif: 'https://media.giphy.com/media/RLyezrCJm6niMeS623/giphy.gif',
         job : 'projects-leader',
-        email : 'luiscu@galileo.edu'
+        email : 'luiscu@galileo.edu',
+        emoji: '🤨'
     },
     {
         name : 'Evelyn Cruz',
         information : 'Labs and workshops instructor with a huge interest in Biomedic field.',
         image : 'eve.png',
         job : 'workshops-leader',
-        email : 'evecruz@galileo.edu'
+        email : 'evecruz@galileo.edu',
+        emoji: '💁🏼'
     },
     {
         name : 'Sofi Cárdenas',
         information : 'Currently working on workshops provided by the lab to school students. Her interests include AR, VR, and automation solutions.',
-        image : 'sofi.png',
+        image : 'sofi.jpg',
+        gif: 'https://media.giphy.com/media/WryTGFOO0hsZ48QWLq/giphy.gif',
         job : 'workshops-assistant',
-        email : 'ascardenas@galileo.edu'
+        email : 'ascardenas@galileo.edu',
+        emoji: '🙄'
     },
     {
         name: 'Holly Milián',
         information: 'Graphic Designer, Illustrator and Amateur Photographer ★ Love cartoons and draw silly things ★',
-        image: 'holly.jpeg',
+        image: 'holly.jpg',
+        gif: 'https://media.giphy.com/media/WoM2yy6nJ9nbdFRjF1/giphy.gif',
         job: 'graphic-designer',
-        email: 'ikuzuhunter@gmail.com'
+        email: 'ikuzuhunter@gmail.com',
+        emoji: '🤷‍♀'
     },
     {
         name : 'Henry Campos',
         information : 'Systems engineer, web developer/designer, AI interested and math lover.',
-        image : 'henry.png',
+        image : 'henry.jpg',
+        gif : 'https://media.giphy.com/media/WS72clqgPYJB7oRUIW/giphy.gif',
         job : 'projects-assistant',
-        email : 'henry.campos@galileo.edu'
+        email : 'henry.campos@galileo.edu',
+        emoji: '👅'
     },
     {
         name : 'Fernando López',
         information : 'Working on workshops with main interest on mechanical design, love to make things work.',
-        image : 'fernando.png',
+        image : 'fernando.jpg',
+        gif: 'https://media.giphy.com/media/PjUuSyj1fBjWNgFqBk/giphy.gif',
         job : 'workshops-assistant',
-        email : 'fernando.lopez@galileo.edu'
+        email : 'fernando.lopez@galileo.edu',
+        emoji: '😶'
     },
     {
         name : 'Jerry Palacios',
         information : 'Working working working working working.',
-        image : 'jerry.png',
+        image : 'jerry.jpg',
+        gif: 'https://media.giphy.com/media/ZFWQMMq9hYiJ7AOlWy/giphy.gif',
         job : 'projects-assistant',
-        email : 'jerry.palacios@galileo.edu'
+        email : 'jerry.palacios@galileo.edu',
+        emoji: '😛'
     }
 ];
 
