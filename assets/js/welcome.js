b2222a5252e24ba5e9f57c1efb6c114ee2c3447d
Luis Jimenez <luisrrjjcc@gmail.com>
2021-01-08 17:02:27 -0600

    bienvenida

---
+++ assets/js/welcome.js
@@ -0,0 +1,105 @@
+var welcomeEntries = [
+    {
+        name : 'Evento: Google I/O Extended',
+        introduction : 'El evento más grande de tecnologías de Google en Guatemala.',
+        thumbnail : 'assets/img/gio/02.png',
+
+        summary : 'El evento contó con 11 conferencistas internacionales. Dos días, dos jornadas cada día, llenas de conferencias técnicas de Machine Learning, Cloud, Mobile, Web y Product Strategy.',
+        
+        description : [],
+        images : ['assets/img/gio/01.png', 'assets/img/gio/02.png', 'assets/img/gio/03.png', 'assets/img/gio/04.png', 'assets/img/gio/05.png'],
+        month : 5,
+        year : 2019
+    },
+
+    {
+        name : 'Evento: IWD GT',
+        introduction : 'Evento conmemorativo del Día Internacional de la Mujer',
+        thumbnail : 'assets/img/iwd/06.png',
+
+        summary : 'Evento con el objetivo de empoderar a mujeres que trabajan o tienen interés en tecnología a través de visibilizar esfuerzos, aumentar vínculos de comunidad y brindar los recursos necesarios para el crecimiento profesional.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/wVIX3BSyAYw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : ['assets/img/iwd/01.png', 'assets/img/iwd/02.png', 'assets/img/iwd/03.png', 'assets/img/iwd/04.png', 'assets/img/iwd/05.png', 'assets/img/iwd/06.png', 'assets/img/iwd/07.png', 'assets/img/iwd/08.png', 'assets/img/iwd/09.png'],
+        month : 3,
+        year : 2020
+    },
+    {
+        name : 'Talleres',
+        introduction : 'Talleres a estudiantes de secundaria.',
+        thumbnail : 'assets/img/talleres/04.png',
+
+        summary : 'Talleres sobre electrónica, inteligencia artificial, diseño 3D, realidad aumentada, entre otros, orientados a estudiantes de secundaria interesados en tecnología.',
+        
+        description : [],
+        images : ['assets/img/talleres/01.png', 'assets/img/talleres/02.png', 'assets/img/talleres/03.png', 'assets/img/talleres/04.png', 'assets/img/talleres/05.png', 'assets/img/talleres/06.png', 'assets/img/talleres/07.png', 'assets/img/talleres/08.png', 'assets/img/talleres/09.png', 'assets/img/talleres/10.png', 'assets/img/talleres/11.png'],
+        month : 8,
+        year : 2018
+    },
+    {
+        name : 'Proyecto: Realidad Aumentada',
+        introduction : 'Proyectar modelos 3D al identificar un patron 2D.',
+        thumbnail : 'assets/img/projs/ar.png',
+
+        summary : 'Dentro del visor de cartón se coloca un teléfono que está ejecutando una aplicación de Android. La aplicación está construída en Unity, y gracias a la librería Vuforia se pueden detectar patrones impresos, y sobre estos proyectar un modelo 3D que será visto por el usuario en la pantalla del celular.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/9VsicGk3a9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 1,
+        year : 2018
+    },
+    {
+        name : 'Proyecto: Hexapod',
+        introduction : 'Robot controlado utilizando Android Things.',
+        thumbnail : 'assets/img/projs/hexapod1.jpg',
+
+        summary : 'Robot de seis patas controlado por una Raspberry Pi, programado con Android Things. Android Things permite programar hardware de la misma forma que se programarían móviles (con Java o Kotlin), permite a través de la Raspberry Pi controlar los motores de las patas del robot y la cámara conectada a este.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZYE2tkUcAm4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 7,
+        year : 2018
+    },
+    {
+        name : 'Proyecto: Realidad Aumentada',
+        introduction : 'Proyectar modelos 3D al identificar un patron 2D.',
+        thumbnail : 'assets/img/projs/ar.png',
+
+        summary : 'Dentro del visor de cartón se coloca un teléfono que está ejecutando una aplicación de Android. La aplicación está construída en Unity, y gracias a la librería Vuforia se pueden detectar patrones impresos, y sobre estos proyectar un modelo 3D que será visto por el usuario en la pantalla del celular.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/9VsicGk3a9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 1,
+        year : 2018
+    },
+    {
+        name : 'Proyecto: Realidad Aumentada',
+        introduction : 'Proyectar modelos 3D al identificar un patron 2D.',
+        thumbnail : 'assets/img/projs/ar.png',
+
+        summary : 'Dentro del visor de cartón se coloca un teléfono que está ejecutando una aplicación de Android. La aplicación está construída en Unity, y gracias a la librería Vuforia se pueden detectar patrones impresos, y sobre estos proyectar un modelo 3D que será visto por el usuario en la pantalla del celular.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/9VsicGk3a9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 1,
+        year : 2018
+    },
+    {
+        name : 'Proyecto: Realidad Aumentada',
+        introduction : 'Proyectar modelos 3D al identificar un patron 2D.',
+        thumbnail : 'assets/img/projs/ar.png',
+
+        summary : 'Dentro del visor de cartón se coloca un teléfono que está ejecutando una aplicación de Android. La aplicación está construída en Unity, y gracias a la librería Vuforia se pueden detectar patrones impresos, y sobre estos proyectar un modelo 3D que será visto por el usuario en la pantalla del celular.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/9VsicGk3a9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 1,
+        year : 2018
+    }
+];
