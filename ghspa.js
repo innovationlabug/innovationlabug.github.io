5d642a71a06da2c565842be5ffc1b7b6e288b66f
Luis Jimenez <luisrrjjcc@gmail.com>
2021-01-11 17:48:34 -0600

    hash welcome -> hash w

--- ghspa.js
+++ ghspa.js
@@ -28,7 +28,7 @@
      l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
               (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
               (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-              '#welcome')
+              '#w')
    }
 
    /* resolve 404 redirects into internal routes */
