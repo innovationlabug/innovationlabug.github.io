a1461d6ab321f68295287a39d3adbd0d80233447
Luis Jimenez <luisrrjjcc@galileo.edu>
2021-01-11 17:41:59 -0600

    Update ghspa.js

--- ghspa.js
+++ ghspa.js
@@ -24,6 +24,7 @@
 
    /* redirect all 404 trafic to index.html */
    function redirect() {
+     console.log(l.hash);
      l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
               (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
               (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
