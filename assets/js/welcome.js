17646324433a101957eb5a6020751fe3321ded55
Luis Jimenez <luisrrjjcc@gmail.com>
2021-01-14 13:58:46 -0600

    thumbs actualizados, cname

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -21,9 +21,9 @@
 var welcomeEntries = [
 
     {
-        name : 'Evento: Google I/O Extended',
+        name : 'Google I/O Extended',
         introduction : 'El evento más grande de tecnologías de Google en Guatemala.',
-        thumbnail : 'assets/img/gio/02.png',
+        thumbnail : 'assets/img/thumbs/gio.jpg',
 
         summary : '',
         
@@ -34,9 +34,9 @@ var welcomeEntries = [
     },
 
     {
-        name : 'Evento: IWD GT',
+        name : 'IWD GT',
         introduction : 'Evento conmemorativo del Día Internacional de la Mujer',
-        thumbnail : 'assets/img/iwd/06.png',
+        thumbnail : 'assets/img/thumbs/iwd.jpg',
 
         summary : '',
         
@@ -48,7 +48,7 @@ var welcomeEntries = [
     {
         name : 'Talleres',
         introduction : 'Talleres a estudiantes de secundaria.',
-        thumbnail : 'assets/img/talleres/04.png',
+        thumbnail : 'assets/img/thumbs/taller.jpg',
 
         summary : '',
         
@@ -58,9 +58,78 @@ var welcomeEntries = [
         year : 2018
     },
     {
-        name : 'Proyecto: Realidad Aumentada',
+        name : 'Bastón plegable para personas con discapacidad visual',
+        introduction : 'Bastón con materiales impresos en 3D o de fácil adquisición.',
+        thumbnail : 'assets/img/thumbs/baston.jpg',
+
+        summary : 'Los bastones plegables para personas con discapacidad visual tienen un costo elevado. Utilizando impresión 3D es posible fabricar uno a bajo costo, y de fácil reparación.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/FMLUwK2qwIU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 10,
+        year : 2020
+    },
+    {
+        name : 'Brazo robot',
+        introduction : 'Brazo robot impreso en 3D controlado por Arduino.',
+        thumbnail : 'assets/img/thumbs/brazo.jpg',
+
+        summary : 'Brazo robot impreso en 3D con siete grados de libertad. Actualmente es controlado utilizando Arduino. Sus servos de alto torque le permitirán mover y clasificar objetos reciclables.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/m6Uh1uH1maE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 12,
+        year : 2020
+    },
+    {
+        name : 'AI para detección de poses',
+        introduction : 'Detección de poses en tiempo real utilizando inteligencia artificial.',
+        thumbnail : 'assets/img/thumbs/pose.jpg',
+
+        summary : 'Una aplicación de la visión por computadora es la detección de poses. El algoritmo nos permite identificar dónde se encuentran las articulaciones de la persona, y se puede comparar estos datos contra una pose conocida. Dispositivos como la Nvidia Jetson tienen suficiente poder para realizar esta tarea en tiempo real.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/1DQjikKqDDI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 12,
+        year : 2020
+    },
+    {
+        name : 'AI para detección de enfermedades',
+        introduction : 'Las inteligencias artificiales pueden ser entrenadas para detectar enfermedades.',
+        thumbnail : 'assets/img/thumbs/ai.jpg',
+
+        summary : 'Los avances de la inteligencia artificial ha brindado grandes oportunidades, como crear AI capaz de generar imágenes como los humanos. Además que brindan la posibilidad de ayudar a la detección de enfermedades de los pacientes. Para esto se deben entrenar con un conjunto de imágenes médicas.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/x0wgev095z0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 12,
+        year : 2020
+    },
+    {
+        name : 'Hexapod',
+        introduction : 'Robot controlado utilizando Android Things.',
+        thumbnail : 'assets/img/thumbs/hexapod.jpg',
+
+        summary : 'Robot de seis patas controlado por una Raspberry Pi, programado con Android Things. Android Things permite programar hardware de la misma forma que se programarían móviles (con Java o Kotlin), permite a través de la Raspberry Pi controlar los motores de las patas del robot y la cámara conectada a este.',
+        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZYE2tkUcAm4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        
+        description : [],
+        images : [],
+        month : 7,
+        year : 2018
+    },
+
+];
+
+/*
+    {
+        name : 'Realidad Aumentada',
         introduction : 'Proyectar modelos 3D al identificar un patron 2D.',
-        thumbnail : 'assets/img/projs/ar.png',
+        thumbnail : 'assets/img/thumbs/ar.jpg',
 
         summary : 'Dentro del visor de cartón se coloca un teléfono que está ejecutando una aplicación de Android. La aplicación está construída en Unity, y gracias a la librería Vuforia se pueden detectar patrones impresos, y sobre estos proyectar un modelo 3D que será visto por el usuario en la pantalla del celular.',
         video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/9VsicGk3a9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -71,22 +140,9 @@ var welcomeEntries = [
         year : 2018
     },
     {
-        name : 'Proyecto: Hexapod',
-        introduction : 'Robot controlado utilizando Android Things.',
-        thumbnail : 'assets/img/projs/hexapod1.jpg',
-
-        summary : 'Robot de seis patas controlado por una Raspberry Pi, programado con Android Things. Android Things permite programar hardware de la misma forma que se programarían móviles (con Java o Kotlin), permite a través de la Raspberry Pi controlar los motores de las patas del robot y la cámara conectada a este.',
-        video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZYE2tkUcAm4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-        
-        description : [],
-        images : [],
-        month : 7,
-        year : 2018
-    },
-    {
         name : 'Impresoras 3D',
         introduction : 'Herramienta para fabricación de prototipos.',
-        thumbnail : 'assets/img/projs/printer.png',
+        thumbnail : 'assets/img/thumbs/printer.jpg',
 
         summary : 'Utiliza carretes de plástico (como PLA o ABS), lo derrite, y va formando una pieza. La pieza se forma conforme el extruser se mueve, línea por línea, y capa por capa. La pieza a formarse puede ser diseñada en distintos programas, como SolidWorks.',
         video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/9VsicGk3a9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -99,7 +155,7 @@ var welcomeEntries = [
     {
         name : 'Oculus Rift',
         introduction : 'Casco de realidad virtual para experiencias inmersivas.',
-        thumbnail : 'assets/img/projs/oculus.png',
+        thumbnail : 'assets/img/thumbs/oculus.jpg',
 
         summary : 'Oculus es un casco de realidad virtual, cuenta con pantalla de alta definición y audífonos para crear una experiencia inmersiva. Los sensores del casco, junto con los sensores sobre la mesa y los controles el usuario tiene, permiten moverse e interactuar con los objetos dentro de los programas de realidad virtual.',
         video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/lbMM6-fbEJ0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -112,7 +168,7 @@ var welcomeEntries = [
     {
         name : 'Mindstorms EV3',
         introduction : 'Kit de Lego para facilitar la construcción de tu primer robot.',
-        thumbnail : 'assets/img/projs/rubik.png',
+        thumbnail : 'assets/img/thumbs/rubik.jpg',
 
         summary : 'Kit de Lego que permite construir y programar robots. Cuenta con varios sensores para obtener información del entorno, y varios motores para ejecutar acciones. Puede programarse mediante programación con bloques, con herramientas como LabView, o con distintos lenguajes de programación.',
         video : '<iframe width="560" height="315" src="https://www.youtube.com/embed/TiIgyVEQQgg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -122,4 +178,4 @@ var welcomeEntries = [
         month : 1,
         year : 2018
     }
-];
+*/
